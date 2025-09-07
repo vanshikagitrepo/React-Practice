69f12acc3fae5eab5df6276d7d6c46fcc20764a7
Vanshika Goel <vanshika.gitrepo@gmail.com>
2025-09-07 22:17:49 +0530

    Inception

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const heading = React.createElement("h1", {}, "Hello world from React !!");
 
 const heading2 = React.createElement("h2", {}, "Jai shree Ram !");
 
-const parent = React.createElement("div", { id:"parent" }, [
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [heading, heading2]),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
