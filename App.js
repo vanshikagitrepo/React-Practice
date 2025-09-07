8938aee98f6839579b3705758fa9d6b2db1e4e3b
Vanshika Goel <vanshika.gitrepo@gmail.com>
2025-09-07 22:50:52 +0530

    heading changed

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-const heading = React.createElement("h1", {}, "Hello world from React !!");
+const heading = React.createElement("h1", {}, "Radhe Radhe !!");
 
 const heading2 = React.createElement("h2", {}, "Jai shree Ram !");
 
@@ -7,3 +7,5 @@ const parent = React.createElement("div", { id: "parent" }, [
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+
+
