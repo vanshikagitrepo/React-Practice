f96ceb5f8c5ecbcfb6485f3c704208cb0479e543
Vanshika Goel <vanshika.gitrepo@gmail.com>
2025-09-08 23:13:37 +0530

    Laying the foundation

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
+// Create a Nested header Element using React.createElement(h1,h2,h3 inside a
+// div with class “title”)
 
-const heading = React.createElement("h1", {}, "Radhe Radhe !!");
+// header>div>h1,h2,h3
 
-const heading2 = React.createElement("h2", {}, "Jai shree Ram !");
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [heading, heading2]),
+const header = React.createElement("div", { className: "title" }, [
+  React.createElement("h1", {}, "Heading 1"),
+  React.createElement("h2", {}, "Heading 2"),
+  React.createElement("h3", {}, "Heading 3"),
 ]);
+
+const addon = () => <h1 className="Header"> Functional compoenent</h1>;
+
+const JSXheader = () => {
+  return (
+    <div>
+      {addon()}
+      <h1>Heading 1 from JSX</h1>
+      <h2>Heading 2 from JSX</h2>
+      <h3>Heading 3 from JSX</h3>
+    </div>
+  );
+};
+
 const root = createRoot(document.getElementById("root"));
-root.render(parent);
-
-
+root.render(<JSXheader />);
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Namste React
 
- <!-- - add scripts for “start” and “build” with parcel commands -->
+
+Create a Header Component from scratch using Functional Components with
+JSX
+○ Add a Logo on left
+○ Add a search bar in middle
+○ Add User icon on right
+○ Add CSS to make it look nice
